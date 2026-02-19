2817caec55c53dcc6a2c8602f49e4f218096a397
Victoria Kastanenka <vkastanenka@gmail.com>
2026-02-18 19:49:52 -0500

    added content container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-base-200">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold text-primary mb-4">It Works!</h1>
-        <button className="btn btn-secondary shadow-lg">DaisyUI Button</button>
-      </div>
-    </div>
-  );
+  return <div className="content-container bg-fuchsia-400">App</div>;
 }
 
 export default App;
