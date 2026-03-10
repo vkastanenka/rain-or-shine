22e073c1a391e6f8eb5c1753ccb2bd4b5af9caaf
Victoria Kastanenka <vkastanenka@gmail.com>
2026-03-09 21:21:00 -0400

    updated BaseWeatherCardDetailed

--- src/features/weather-forecast/components/base-weather-card-detailed/BaseWeatherCardDetailed.tsx
+++ src/features/weather-forecast/components/base-weather-card-detailed/BaseWeatherCardDetailed.tsx
@@ -7,7 +7,114 @@ import {
   WiRaindrop,
   WiWindDeg,
 } from "@/assets/icons/erikflowers-weather-icons";
-import { BaseWeatherCardDetailedVariable } from "../base-weather-card-detailed-variable";
+import { type ComponentType } from "react";
+
+// --- Types ---
+
+interface IconProps {
+  className?: string;
+}
+
+interface WeatherVariableProps {
+  label: string;
+  value: string | number;
+  Icon: ComponentType<IconProps>;
+  className?: string;
+}
+
+// --- Sub-Components ---
+
+export const BaseWeatherCardDetailedContainer = ({
+  children,
+  className,
+  ...props
+}: any) => (
+  <Card className={cn("p-2 sm:p-3 xl:p-4 w-full", className)} {...props}>
+    {children}
+  </Card>
+);
+
+export const WeatherStat = ({
+  label,
+  value,
+  Icon,
+  className,
+}: WeatherVariableProps) => (
+  <FlexRow align="center" gap={1} className={cn("min-w-fit", className)}>
+    <FlexRow align="center" gap={1}>
+      <Icon className="w-4 sm:w-6 xl:w-7 fill-base-content" />
+      <Text
+        type={{ base: "caption", sm: "body2", xl: "body1" }}
+        className="whitespace-nowrap"
+      >
+        {label}
+      </Text>
+    </FlexRow>
+    <Text
+      type={{ base: "caption", sm: "body2", xl: "body1" }}
+      className="font-medium"
+    >
+      {value}
+    </Text>
+  </FlexRow>
+);
+
+// --- Layout Components ---
+
+export const BaseWeatherCardDetailedMainData = ({
+  temperature,
+  condition,
+  apparentTemp,
+  Icon,
+}: any) => (
+  <Flex
+    gap={{ base: 0, sm: 2 }}
+    direction={{ base: "col", sm: "row" }}
+    align={{ base: "start", sm: "center" }}
+  >
+    <FlexRow gap={2} align="center">
+      <Icon className="w-8 sm:w-12 xl:w-16" />
+      <Text type={{ base: "headline3", sm: "headline2" }}>{temperature}</Text>
+    </FlexRow>
+    <Flex direction={{ base: "col-reverse", sm: "col" }}>
+      <Text type={{ base: "body2", sm: "body1", xl: "large" }}>
+        {condition}
+      </Text>
+      <Text
+        type={{ base: "caption", sm: "body2", xl: "body1" }}
+        className="opacity-80"
+      >
+        {apparentTemp}
+      </Text>
+    </Flex>
+  </Flex>
+);
+
+export const BaseWeatherCardDetailedStats = ({
+  wind,
+  windGust,
+  humidity,
+  pop,
+  rain,
+}: any) => (
+  <Flex
+    direction={{ base: "col", lg: "row" }}
+    gap={{ base: 1, sm: 2 }}
+    className="flex-wrap"
+  >
+    <FlexRow gap={2} wrap>
+      <WeatherStat Icon={WiWindDeg} label="Wind" value={wind} />
+      <WeatherStat Icon={WiStrongWind} label="Gust" value={windGust} />
+      <WeatherStat Icon={WiHumidity} label="Hum." value={humidity} />
+    </FlexRow>
+    <FlexRow gap={2} wrap>
+      <WeatherStat Icon={WiRain} label="P.O.P." value={pop} />
+      {rain && <WeatherStat Icon={WiRaindrop} label="Rain" value={rain} />}
+    </FlexRow>
+  </Flex>
+);
+
+// --- Main Component ---
 
 export const BaseWeatherCardDetailed = ({
   timeLabel,
@@ -23,87 +130,31 @@ export const BaseWeatherCardDetailed = ({
   ...props
 }: any) => {
   return (
-    <Card className={cn("p-2", "sm:p-3", "xl:p-4", "w-full")} {...props}>
-      <FlexRow
-        gap={2}
-        justify="between"
-        align="center"
-        className={cn("max-w-110", "sm:max-w-3xl", "xl:max-w-5/6")}
-      >
-        <FlexCol>
+    <BaseWeatherCardDetailedContainer {...props}>
+      <FlexRow gap={4} justify="between" align="center" className="w-full">
+        <FlexCol gap={1}>
           <Text
             type={{ base: "body2", sm: "body1", xl: "large" }}
             className="font-bold"
           >
             {timeLabel}
           </Text>
-          <Flex
-            direction={{ base: "col", sm: "row" }}
-            gap={{ base: 0, sm: 2 }}
-            align={{ base: "start", sm: "center" }}
-          >
-            <FlexRow gap={2} align="center">
-              <ConditionIcon className="w-8 sm:w-15, xl:w-18"></ConditionIcon>
-              <Text type={{ base: "headline3", sm: "headline2" }}>
-                {temperatureLabel}
-              </Text>
-            </FlexRow>
-            <Flex direction={{ base: "col-reverse", sm: "col" }} gap={0}>
-              <Text type={{ base: "body2", sm: "body1", xl: "large" }}>
-                {conditionLabel}
-              </Text>
-              <Text
-                type={{
-                  base: "caption",
-                  sm: "body2",
-                  xl: "body1",
-                }}
-              >
-                {apparentTemperatureLabel}
-              </Text>
-            </Flex>
-          </Flex>
+          <BaseWeatherCardDetailedMainData
+            temperature={temperatureLabel}
+            condition={conditionLabel}
+            apparentTemp={apparentTemperatureLabel}
+            Icon={ConditionIcon}
+          />
         </FlexCol>
-        <Flex direction={{ base: "col", sm: "row" }} gap={{ base: 0, sm: 2 }}>
-          <FlexRow
-            gap={{ base: 1, sm: 2 }}
-            className={cn("w-full", "sm:w-auto")}
-          >
-            <BaseWeatherCardDetailedVariable
-              MainIcon={WiWindDeg}
-              nameLabel={"Wind"}
-              value={windValueLabel}
-            />
-            <BaseWeatherCardDetailedVariable
-              MainIcon={WiStrongWind}
-              mainLabel={"Wind Gust"}
-              secondaryLabel={windGustValueLabel}
-            />
-            <BaseWeatherCardDetailedVariable
-              Icon={WiHumidity}
-              mainLabel={"Humidity"}
-              secondaryLabel={humidityValueLabel}
-            />
-          </FlexRow>
-          <FlexRow
-            gap={{ base: 1, sm: 2 }}
-            className={cn("w-full", "sm:w-auto")}
-          >
-            <BaseWeatherCardDetailedVariable
-              MainIcon={WiRain}
-              mainLabel={"P.O.P."}
-              secondaryLabel={precipitationChanceValueLabel}
-            />
-            {rainValueLabel && (
-              <BaseWeatherCardDetailedVariable
-                Icon={WiRaindrop}
-                mainLabel={"Rain"}
-                secondaryLabel={rainValueLabel}
-              />
-            )}
-          </FlexRow>
-        </Flex>
+
+        <BaseWeatherCardDetailedStats
+          wind={windValueLabel}
+          windGust={windGustValueLabel}
+          humidity={humidityValueLabel}
+          pop={precipitationChanceValueLabel}
+          rain={rainValueLabel}
+        />
       </FlexRow>
-    </Card>
+    </BaseWeatherCardDetailedContainer>
   );
 };
