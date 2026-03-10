da5d6b8d9710ac08711e9055570a82d0c9b8d66f
Victoria Kastanenka <vkastanenka@gmail.com>
2026-03-10 19:25:58 -0400

    added tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,20 @@ import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    ,
+    react(),
+    svgr(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
