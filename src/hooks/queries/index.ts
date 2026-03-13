944bb88a0d597ccf8574c07686c61ef93260b2db
Victoria Kastanenka <vkastanenka@gmail.com>
2026-03-12 22:29:14 -0400

    removed redundant hooks files

--- src/hooks/queries/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./open-meteo/useWeatherForecast";
-export * from "./open-meteo/useHistoricalWeather";
-export * from "./open-meteo/useSeasonalForecast";
-export * from "./open-meteo/useGeocoding";
-export * from "./open-meteo/keys";
